ef16b81674963602642d96f8607c730cb5f7a5f7
Peter Edin <Peter.a.edin@gmail.com>
2015-07-05 21:34:06 -0400

    Update openCVScript.sh

--- openCVScript.sh
+++ openCVScript.sh
@@ -43,4 +43,4 @@ sudo ldconfig
 
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
-sudo apt-get install sublime-text-installer
+sudo apt-get -y install sublime-text-installer
