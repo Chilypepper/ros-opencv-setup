5de28deac08ab32b59ef8da9395076f6487fc337
Peter Edin <Peter.a.edin@gmail.com>
2015-07-28 15:38:14 -0400

    added a -y for a "no hands" approach

--- openCVScript.sh
+++ openCVScript.sh
@@ -18,7 +18,7 @@ sudo apt-get -y install python-vtk
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
 sudo apt-get update
-sudo apt-get install ros-jade-desktop-full
+sudo apt-get install -y ros-jade-desktop-full
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/jade/setup.bash" >> ~/.bashrc
